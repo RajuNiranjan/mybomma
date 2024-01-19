92f9b7c2dd46636161e4e04f4fad12258e026207
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-01-19 10:53:42 +0530

    created footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { GlobalContextProvider } from "@/context/context";
 import Navbar from "@/components/navbar/navbar";
+import Footer from "@/components/footer/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
           <div className="container m-auto flex flex-col h-screen justify-between">
             <Navbar />
             <div className="px-5">{children}</div>
+            <Footer />
           </div>
         </GlobalContextProvider>
       </body>
